e7bd24cc507463d930d92cf3b5b3fc44f8565e6d
youne <younes.vg20@gmail.com>
2021-05-16 22:38:14 +0200

    first commit

---
+++ src/main/java/com/example/managementappfinal/ManagementappfinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.managementappfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementappfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementappfinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/managementappfinal/ManagementappfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managementappfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementappfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
